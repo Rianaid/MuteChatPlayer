7726e383e89365a65bf0c4b7224bb88b90b8051b
Rianaid <ust-ilimsck@ya.ru>
2024-06-03 08:29:08 +0800

    Fix name of coroutine.

--- Utils/MutePlayersSystem.cs
+++ Utils/MutePlayersSystem.cs
@@ -52,9 +52,9 @@ namespace MuteChatPlayer.Utils
         void Start()
         {
             Load_Mute_List();
-            StartCoroutine(UpdateDurabilityInSpecialStorage().WrapToIl2Cpp());
+            StartCoroutine(UpdateMuteList().WrapToIl2Cpp());
         }
-        private IEnumerator UpdateDurabilityInSpecialStorage()
+        private IEnumerator UpdateMuteList()
         {
             while (true)
             {
